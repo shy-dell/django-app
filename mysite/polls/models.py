e0658b36bf3ed136edf44c4d153580f0d9e2be43
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-23 11:47:08 -0400

    update models to define str function and custom function

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,14 +1,24 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200) #CharField has required arguments i.e., max length
     pub_date = models.DateTimeField("date published") #these will be the columns in our db
+    
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     #when a row in the parent table is deleted, all associated values in the child table(s) with the same foreign key will be deleted (CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
 
 
